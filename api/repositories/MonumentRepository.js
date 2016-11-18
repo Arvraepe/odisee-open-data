5fb3b216e2e77aa28982067b08de5730074aa6cc
Arne Van Raepenbusch <arvraepe@gmail.com>
2016-11-18 07:38:18 +0100

    added the data origin link

--- api/repositories/MonumentRepository.js
+++ api/repositories/MonumentRepository.js
@@ -4,7 +4,7 @@
 const fetch = require('node-fetch');
 
 let data = {};
-// TODO: fetch data
+// TODO: fetch data at 'http://ws.brugis.irisnet.be/geoserver/INSPIREFR/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=INSPIREFR%3AMonuments_classes&outputFormat=JSON&maxFeatures=50'Adde
 
 // TODO: create functions
 
